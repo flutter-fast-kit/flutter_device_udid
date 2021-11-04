d14dfbd050d0011ddc19e67c3cd425b468648c85
Yong Hui <liuyonghui.job@gmail.com>
2021-11-04 12:52:34 +0800

    Use application context

--- android/src/main/java/io/flutterfastkit/flutter_device_udid/FlutterDeviceUdidPlugin.java
+++ android/src/main/java/io/flutterfastkit/flutter_device_udid/FlutterDeviceUdidPlugin.java
@@ -1,59 +1,40 @@
 package io.flutterfastkit.flutter_device_udid;
 
-import android.app.Activity;
+import android.content.Context;
 
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.embedding.engine.plugins.activity.ActivityAware;
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
-public class FlutterDeviceUdidPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
-  private MethodChannel channel;
-  private Activity mActivity;
+public class FlutterDeviceUdidPlugin implements FlutterPlugin, MethodCallHandler {
+    private MethodChannel channel;
+    private Context applicationContext;
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_device_udid");
-    channel.setMethodCallHandler(this);
-  }
-
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getDeviceId")) {
-      String deviceId = UniversalID.getUniversalID(mActivity);
-      result.success(deviceId);
-    } else {
-      result.notImplemented();
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        applicationContext = flutterPluginBinding.getApplicationContext();
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_device_udid");
+        channel.setMethodCallHandler(this);
     }
-  }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-  }
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        if (call.method.equals("getDeviceId")) {
+            String deviceId = UniversalID.getUniversalID(applicationContext);
+            result.success(deviceId);
+        } else {
+            result.notImplemented();
+        }
+    }
 
-  @Override
-  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
-    mActivity = binding.getActivity();
-  }
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+        applicationContext = null;
+    }
 
-  @Override
-  public void onDetachedFromActivityForConfigChanges() {
-    onDetachedFromActivity();
-  }
-
-  @Override
-  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
-    onAttachedToActivity(binding);
-  }
-
-  @Override
-  public void onDetachedFromActivity() {
-    mActivity = null;
-  }
 }
